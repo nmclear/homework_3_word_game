b39b7021ef400b45ff997f7bfa7d2a757d96ca20
nmclear <nickclear22@gmail.com>
2018-05-10 18:44:33 -0700

    created function for selectGameWord. Avoids global var gameWord.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,8 +17,6 @@ var wordBank = [
     "paper"
 ];
 
-// randomly selected guessWord from wordBank.
-var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 
 //create variables to store: correct guess, incorrect letters, guesses remaining, total win, total loss.
@@ -36,11 +34,18 @@ var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 //FUNCTIONS
 //========================================================================================================================
 
+// randomly selected guessWord from wordBank.
+function selectGameWord(){    
+    var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    return gameWord;
+}
+
+
 function startGame() {
 
-         
+    
     //Split gameWord into array of characters
-    var gameWordChar = gameWord.split("");
+    var gameWordChar = selectGameWord().split("");
 
     //Create blankSpaces
     var display = [];
@@ -48,6 +53,7 @@ function startGame() {
         console.log(gameWordChar[i]);
         display += "_";
     }
+
     document.querySelector("#display").innerHTML = display;
 
 
@@ -70,9 +76,9 @@ function checkGuess(){
 
         checkGuessBank(userGuess);
 
-
         if(gameWord.indexOf(userGuess) !== -1) {
             correctLetter++;
+            
         } else {
             wrongLetter++;
             guessesRemaining--;
@@ -84,9 +90,7 @@ function checkGuess(){
         "<p>You chose: " + userGuess + "</p>" +
         "<p>Guesses Remaining: " + guessesRemaining + "</p>" +
         "<p>Correct Letters: " + correctLetter + "</p>" +
-        "<p>Wrong Letters: " + wrongLetter + "</p>" +
-        "<p>Wins: " + totalWin + "</p>" +
-        "<p>Losses: " + totalLoss + "</p>";
+        "<p>Wrong Letters: " + wrongLetter + "</p>"
 
         // set the inner html contents of #game div to html string.
         document.querySelector("#game").innerHTML = html;
@@ -96,9 +100,10 @@ function checkGuess(){
 }
 
 function checkGuessBank(value){
+    //if guess is not in guess bank, add character to guess bank.
     if(guessBank.indexOf(value) === -1) {
         guessBank.push(value);
-        console.log(guessBank);
+        console.log("guessBank: " + guessBank);
     }
     else {
         checkGuess();
@@ -107,15 +112,14 @@ function checkGuessBank(value){
 
 
 
-function replaceLetter() {
-
+function replaceLetter(value) {
+    
 }
 
 
 function checkGameOver() {
     if(guessesRemaining === 0) {
         alert("Game Over");
-        totalLoss++;
         document.location.reload();
     }
 }
@@ -132,9 +136,7 @@ function checkGameOver() {
 
 
 
-
-
-// if all letters guessed [end the game] [add 1 to win] [prompt for new game]
+// if all letters guessed [end the game] [prompt for new game]
 
 // create function for new game to reset code.
 
